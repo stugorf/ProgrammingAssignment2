9cf21735a4bf17b15039dc3ca21094e61b113b96
stugorf <david@froguts.com>
2015-03-22 12:42:52 -0700

    Final code submit

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,64 @@
 # Overall Description:
-#   
+#   A pair of functions that create and cache the inverse of a matrix. The functions
+#   take advantage of R's scoping rules to store/cache inverses of a supplied 
+#   matrix once it has been computed and provides retrieval functions to reduce 
+#   computation times rather than requiring inverses be calculated for each matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-# Description:
-#   
+    # Description:
+    #   This function creates a special "matrix" object that can cache its inverse.
+    # Args:
+    #   x: a square invertabile matrix, default equals empty matrix 
+    # Returns:
+    #   a matrix and references to internal public functions
 
-# Args:
-#   directory: is a character vector of length 1 indicating
-#   the location of the CSV files
-
-#   threshold: is a numeric vector of length 1 indicating the
-#   number of completely observed observations (on all
-#   variables) required to compute the correlation between
-#   nitrate and sulfate; the default is 0
-
-# Returns:
-#   a numeric vector of correlations
-
+    unCachedMatrix <- x
+    cachedMatrix <- NULL
+    set <- function(y){
+        cachedMatrix <<- NULL
+    }
+    get <- function() unCachedMatrix
+    setCachedMatrix <- function(invertedMatrix) cachedMatrix <<- invertedMatrix
+    getCachedMatrix <- function() cachedMatrix
+    
+    methods = list(set = set, get = get, setCachedMatrix = setCachedMatrix, 
+         getCachedMatrix = getCachedMatrix)
 }
 
 
 cacheSolve <- function(x, ...) {
-# Description:
-#   
-
-# Args:
-#   directory: is a character vector of length 1 indicating
-#   the location of the CSV files
-
-#   threshold: is a numeric vector of length 1 indicating the
-#   number of completely observed observations (on all
-#   variables) required to compute the correlation between
-#   nitrate and sulfate; the default is 0
-
-# Returns:
-#   a matrix that is the inverse of 'x'
+    # Description:
+    #   This function computes the inverse of the "matrix" returned by makeCacheMatrix
+    # Args:
+    #   x: a square invertible matrix reference which was created using makeCacheMatrix
+    #   ...: additional arguments
+    # Returns:
+    #   an inverted matrix OR if a cached matrix exist this function retrieves a
+    #   cached inverted matrix
     
+    cachedMatrix = x$getCachedMatrix()
+    if(!is.null(cachedMatrix)){
+        message("getting cached matrix")
+        return(cachedMatrix)
+    } else{
+        message("cached matrix not found, solving inverse and cacheing")
+        unCachedMatrix <- x$get()
+        invertedMatrix <- solve(unCachedMatrix)
+        x$setCachedMatrix(invertedMatrix)
+        invertedMatrix
+    }
 }
+
+### CODE BELOW FOR TESTING ###
+#   Test making matrix
+newMatrix <- matrix(c(-1, -2, 1, 1), 2,2)
+m <- makeCacheMatrix(newMatrix)
+m$get()
+
+#   Test creating inverse matrix
+inv <- cacheSolve(m)
+inv
+
+#   Test retrieving cached matrix
+inv <- cacheSolve(m)
+inv
